e98535973c7cc3ccb6f79352ff5cc6b99cb217e5
thienx <trinhthien426@gmail.com>
2025-11-09 14:01:08 +0700

    choose  sessionDate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,8 @@ android {
     }
 
     buildTypes {
-        release {
+        // Dùng getByName để cấu hình một build type đã có (như "release")
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -25,11 +26,13 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
 
+    // ⭐ SỬA LỖI 1: Cú pháp .kts dùng dấu '=' và 'isCoreLibraryDesugaringEnabled'
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true // Sửa từ 'coreLibraryDesugaringEnabled'
+    }
 }
 
 dependencies {
@@ -41,12 +44,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
     // Retrofit for REST API calls
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-// Converter GSON for automatic JSON <-> Java object mapping
+    // Converter GSON for automatic JSON <-> Java object mapping
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
-
+    // ⭐ SỬA LỖI 2: Cú pháp .kts dùng dấu ngoặc đơn ()
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
