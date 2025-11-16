3e7ca32a0342b3d209adee42bca4304d07f5b11c
Nhat-Original <109066694+Nhat-Original@users.noreply.github.com>
2025-11-16 15:25:06 +0700

    fixed some chatbox logics, added currentUserId field to RegisterLoginResponse

--- app/src/main/java/com/example/login/model/RegisterLoginResponse.java
+++ app/src/main/java/com/example/login/model/RegisterLoginResponse.java
@@ -3,7 +3,9 @@ package com.example.login.model;
 public class RegisterLoginResponse{
     private String message;
     private String token;
+    private Integer currentUserId;
 
     public String getMessage() { return message; }
     public String getToken() { return token; }
+    public Integer getCurrentUserId() { return currentUserId; }
 }
